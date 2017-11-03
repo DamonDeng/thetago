c5ea9ac1050a652fde3a47cf0d3ca110bf7f5a3f
dengmingxuan@hotmail.com <dengmingxuan@hotmail.com>
2017-11-03 13:08:15 +0800

    Download testing code

--- download_kgs_data.py
+++ download_kgs_data.py
@@ -1,8 +1,16 @@
-
-
 from data_loader.index_processor import KGSIndex
+from data_loader.kgs_downloader import KGSDownloader
+
+from gosgf import Sgf_game
+
+import gzip
+import shutil
+import tarfile
+import os
+import os.path
 
 print('Started to download KGS data...')
 
-index = KGSIndex(data_directory='data')
-index.download_files()
+downloader = KGSDownloader()
+
+downloader.retrieve_files()
