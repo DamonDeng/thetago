ea2e17ef9632422cf20bc367dc4ad3c5982f5dc2
dengmingxuan@hotmail.com <dengmingxuan@hotmail.com>
2017-11-06 16:03:03 +0800

    new server cnn network architect

--- mxnet_server_training.py
+++ mxnet_server_training.py
@@ -4,41 +4,6 @@ from data_loader.sgf_iter import SimpleIter, SimulatorIter, SGFIter
 import logging
 
 
-def simple_mlp():
-
-  print('mxnet training with MLP  0.01')
-  
-  num_classes = 361
-  net = mx.sym.Variable('data')
-  net = mx.sym.FullyConnected(data=net, name='fc1', num_hidden=64)
-  net = mx.sym.Activation(data=net, name='relu1', act_type="relu")
-  net = mx.sym.FullyConnected(data=net, name='fc2', num_hidden=num_classes)
-  net = mx.sym.SoftmaxOutput(data=net, name='softmax')
-  print(net.list_arguments())
-  print(net.list_outputs())
-
-
-
-
-  logging.basicConfig(level=logging.INFO)
-
- 
-  data_iter = SGFIter(sgf_directory='data/train', batch_size=32, file_limit = 200)
- 
- 
-  print (str(data_iter.provide_data))
-  print (str(data_iter.provide_label))
-
-  c = data_iter.next()
-
-  print (str(c))
-  prefix = 'checkpoint/mxnet_thetago'
-  mod = mx.mod.Module(symbol=net)
-  mod.fit(data_iter, 
-          num_epoch=100, 
-          batch_end_callback=mx.callback.Speedometer(32, 20),
-          epoch_end_callback=mx.callback.do_checkpoint(prefix))
-
 def simple_CNN():
 
   print('mxnet training with CNN  0.01')
@@ -47,18 +12,19 @@ def simple_CNN():
 
   data = mx.symbol.Variable('data')
   # first conv layer
-  conv1 = mx.sym.Convolution(data=data, kernel=(3,3), num_filter=20)
+  conv1 = mx.sym.Convolution(data=data, kernel=(4,4), pad=(1,1), num_filter=20)
   tanh1 = mx.sym.Activation(data=conv1, act_type="tanh")
-  # pool1 = mx.sym.Pooling(data=tanh1, pool_type="max", kernel=(2,2), stride=(2,2))
+  pool1 = mx.sym.Pooling(data=tanh1, pool_type="max", kernel=(2,2), stride=(1,1))
   # second conv layer
-  conv2 = mx.sym.Convolution(data=tanh1, kernel=(3,3), num_filter=50)
+  conv2 = mx.sym.Convolution(data=pool1, kernel=(4,4), pad=(1,1), num_filter=50)
   tanh2 = mx.sym.Activation(data=conv2, act_type="tanh")
-  # pool2 = mx.sym.Pooling(data=tanh2, pool_type="max", kernel=(2,2), stride=(2,2))
+  pool2 = mx.sym.Pooling(data=tanh2, pool_type="max", kernel=(2,2), stride=(1,1))
   # first fullc layer
-  conv3 = mx.sym.Convolution(data=tanh2, kernel=(3,3), num_filter=70)
+  conv3 = mx.sym.Convolution(data=pool2, kernel=(4,4), num_filter=70)
   tanh3 = mx.sym.Activation(data=conv3, act_type="tanh")
+  pool3 = mx.sym.Pooling(data=tanh3, pool_type="max", kernel=(2,2), stride=(2,2))
   
-  flatten = mx.sym.Flatten(data=tanh3)
+  flatten = mx.sym.Flatten(data=pool3)
   fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=500)
   tanh3 = mx.sym.Activation(data=fc1, act_type="tanh")
   # second fullc
@@ -73,10 +39,11 @@ def simple_CNN():
   print (str(data_iter.provide_data))
   print (str(data_iter.provide_label))
   
-  prefix = 'checkpoint/mxnet_thetago_CNN'
+  prefix = 'checkpoint/thetago_CNN2'
 
   devices = mx.cpu(0)
-  # device = mx.gpu(0)
+  # devices = mx.gpu(0)
+  # devices = [mx.gpu(0),mx.gpu(1),mx.gpu(2),mx.gpu(3),mx.gpu(4),mx.gpu(5),mx.gpu(6),mx.gpu(7)]
 
   mod = mx.mod.Module(symbol=cnn_net,
                       context=devices)
