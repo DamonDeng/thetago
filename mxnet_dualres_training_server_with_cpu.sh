6535d4253db7174c3144656308bbcd4676307b45
dengmingxuan@hotmail.com <dengmingxuan@hotmail.com>
2018-04-19 15:50:10 +0900

    added server testing shell with cpu

---
+++ mxnet_dualres_training_server_with_cpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+python ./mxnet_dualres_training.py train \
+--data data/ \
+--network network.dual_resnet \
+--prefix checkpoint/thetago_dual_res_17L_19res \
+--epoche 100 \
+--learningrate 0.1 \
+--batchsize 4 \
+--evalmetric mse \
+--processor ZeroDualResProcessor 
+
+
+
